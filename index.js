6f3ec7bb734eb000cb6055481afb0f26415e75e4
Victor Briones <vbrionesparra@gmail.com>
2023-09-02 01:04:13 -0400

    cors

--- index.js
+++ index.js
@@ -16,11 +16,31 @@ mongoose.connect(process.env.MONGO_URI + 'tienda');
 
 const port = process.env.PORT;
 
-const corsOptions={
-    origin:[process.env.FRONTEND_URL,process.env.FRONTEND_URL_DEV],
-    optionsSuccessStatus:200
-}
-app.use(cors(corsOptions))
+// const corsOptions={
+//     origin:[process.env.FRONTEND_URL,process.env.FRONTEND_URL_DEV],
+//     optionsSuccessStatus:200
+// }
+// app.use(cors(corsOptions))
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://fashionstorecl.netlify.app/"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+
+  next();
+});
+
 app.use(express.json())
 app.use('/users',userRoutes)
 app.use('/payment',paymentRoutes)
